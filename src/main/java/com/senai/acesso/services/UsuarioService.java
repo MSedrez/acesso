583ffae994ba41c2e6fd21194f2de593000c5f0e
Marlos Sedrez <marlos.sedrez@gmail.com>
2026-09-16 20:05:51 -0300

    Commit da versao 2

--- src/main/java/com/senai/acesso/services/UsuarioService.java
+++ src/main/java/com/senai/acesso/services/UsuarioService.java
@@ -4,30 +4,56 @@ import com.senai.acesso.dtos.UsuarioDto;
 import com.senai.acesso.models.UsuarioEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UsuarioService {
 
+    //--Declaração da lista de usuários na classe (escopo global da classe UsuarioService
+    private List<UsuarioEntity> lista = new ArrayList<UsuarioEntity>();
+
     //--Metodo para cadastrar o usuário que recebe os dados do usuário no DTO!!!
-    public boolean cadastrar(UsuarioDto usuario){
+    public boolean cadastrar(UsuarioDto usuarioDto) {
+
+        //--Preciso verificar se o usuário já foi cadastrado
+        //--Estrutura de repetição para lista de objetos
+        for (UsuarioEntity usuario : lista) {
+
+            //--Para cada item da lista verifica se o cpf do item da lista é igual ao cpf do usuário do DTO
+            if (usuario.getCpf().equals(usuarioDto.getCpf())) {
+                //--Se for igual, significa que o cpf já foi cadastrado na lista
+                return false;
+            }
+        }
+
+        //-Valida dados preenchidos ( campos obrigatórios )
+        if (usuarioDto.getCpf().equals("")) {
+            //--Retorna para o controller se o cpf não foi informado
+            return false;
+        }
+        if (usuarioDto.getNome().equals("")){
+            //--Retorna para o controller se o nome não foi informado
+            return false;
+        }
+        if (usuarioDto.getLogin().equals("")){
+            //--Retorna para o controller se o login não foi informado
+            return false;
+        }
 
         //--Converter os dados do DTO para o ENTITY
         UsuarioEntity usuarioEntity = new UsuarioEntity();
-        usuarioEntity.setCpf(usuario.getCpf());
-        usuarioEntity.setNome(usuarioEntity.getNome());
-        usuarioEntity.setLogin(usuario.getLogin());
-        usuarioEntity.setSenha(usuario.getSenha());
 
-        //--Preciso verificar se o usuário já foi cadastrado
+        //-- ou seja obtém o valor dos dados do DTO e atribui para um novo objeto do tipo Entity
+        usuarioEntity.setCpf(usuarioDto.getCpf());
+        usuarioEntity.setNome(usuarioDto.getNome());
+        usuarioEntity.setLogin(usuarioDto.getLogin());
+        usuarioEntity.setSenha(usuarioDto.getSenha());
 
-        //--Se o usuário já esta cadastrado eu retorno false
+        //---Adiciona na lista o usuário entity com os dados vindos do DTO
+        lista.add(usuarioEntity);
 
-        //-Valida dados preenchidos ( campos obrigatórios )
-
-        // -- se não tiver algum dado preenchido obrigatório retorna false
-
-        //---Se não foi cadastrado preciso então cadatrar o usuário
-
-        //--Se deu certo cadatrar o usuário eu retrorno verdadeiro (true)
+        //--Se deu certo cadastrar o usuário eu retrorno verdadeiro (true)
         return true;
     }
 
